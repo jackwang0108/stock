cf4dbb70a914efa8c7f5f5f94c8f954a500d0c82
Jack <jack4shihong@gmail.com>
2025-04-09 19:26:28 +0800

    add stock basic

--- src/types.py
+++ src/types.py
@@ -26,7 +26,7 @@ class ProAPI(ABC):
         daily 获取A股日线行情
 
         Args:
-            ts_code (str): 股票代码（支持多个股票同时提取，逗号分隔）
+            ts_code (str): tushare股票代码（支持多个股票同时提取，逗号分隔）
             trade_date (str): 交易日期（YYYYMMDD）
             start_date (str): 开始日期(YYYYMMDD)
             end_date (str): 结束日期(YYYYMMDD)
@@ -50,13 +50,14 @@ class ProAPI(ABC):
 
     @abstractmethod
     def trade_cal(
-        exchange: Literal["SSE", "SZSE", "CFFEX", "SHFE", "CZCE", "DCE", "INE"],
+        self,
         start_date: str,
         end_date: str,
         is_open: Literal["0", "1"],
+        exchange: Literal["SSE", "SZSE", "CFFEX", "SHFE", "CZCE", "DCE", "INE"],
     ) -> pd.DataFrame:
         """
-        trade_cal 获取各大交易所交易日历数据, 默认提取的是上交所
+        trade_cal 获取指定交易所交易日历数据, 默认提取的是上交所
 
         Args:
             exchange (Literal[&quot;SSE&quot;, &quot;SZSE&quot;, &quot;CFFEX&quot;, &quot;SHFE&quot;, &quot;CZCE&quot;, &quot;DCE&quot;, &quot;INE&quot;]): 交易所代号, SSE上交所, SZSE深交所, CFFEX中金所, SHFE上期所, CZCE郑商所, DCE大商所, INE上能源
@@ -71,5 +72,54 @@ class ProAPI(ABC):
             cal_date	    str	    Y	    日历日期
             is_open	        str	    Y	    是否交易 0休市 1交易
             pretrade_date	str	    Y	    上一个交易日
+        """
+        pass
+
+    @abstractmethod
+    def stock_basic(
+        self,
+        fields: str,
+        name: Optional[str] = None,
+        ts_code: Optional[str] = None,
+        is_hs: Optional[Literal["N", "H", "S"]] = None,
+        market: Optional[Literal["主板", "创业板", "科创板", "CDR", "北交所"]] = None,
+        exchange: Optional[
+            Literal["SSE", "SZSE", "CFFEX", "SHFE", "CZCE", "DCE", "INE"]
+        ] = "SSE",
+        list_status: Optional[Literal["L", "D", "P"]] = "L",
+    ) -> pd.DataFrame:
+        """
+        stock_basic 获取股票基本信息
+        该接口提供了股票的基本信息，包括股票代码、名称、上市日期、退市日期、是否沪深港通标的等信息。
+
+        Args:
+            fields (str): 获取的基本信息字段列表，多个字段用逗号分隔, 可选字段见返回值
+            name (Optional[str], optional): 指定获取基础信息的股票的名称.
+            ts_code (Optional[str], optional): 指定获取基础信息的股票的tushare股票代码.
+            is_hs (Optional[Literal[&quot;N&quot;, &quot;H&quot;, &quot;S&quot;]], optional): 获取沪深港通股票的基础信息, &quot;N&quot; 获取非沪深港通股票的基础信息, &quot;H&quot; 获得沪股通股票的基础信息, &quot;S&quot; 获得深股通股票的基础信息. 留空表示获得所有股票的基础信息.
+            market (Optional[Literal[&quot;&quot;, &quot;主板&quot;, &quot;创业板&quot;, &quot;科创板&quot;, &quot;CDR&quot;, &quot;北交所&quot;], optional): 获得指定板块中的股票的基础信息.
+            exchange (Optional[ Literal[&quot;SSE&quot;, &quot;SZSE&quot;, &quot;CFFEX&quot;, &quot;SHFE&quot;, &quot;CZCE&quot;, &quot;DCE&quot;, &quot;INE&quot;] ], optional): 获得指定交易所中的股票的基础信息.
+            list_status (Optional[Literal[&quot;L&quot;, &quot;D&quot;, &quot;P&quot;]], optional): 获得指定状态的股票的基础信息. &quot;L&quot; 正常交易, &quot;D&quot; 退市, &quot;P&quot; 暂停上市.
+
+        Returns:
+            pd.DataFrame: 股票基本信息数据表格
+                名称	        类型	默认显示	描述
+                ts_code	        str	    Y	    TS代码
+                symbol	        str	    Y	    股票代码
+                name	        str	    Y	    股票名称
+                area	        str	    Y	    地域
+                industry	    str	    Y	    所属行业
+                fullname	    str	    N	    股票全称
+                enname	        str	    N	    英文全称
+                cnspell	        str	    Y	    拼音缩写
+                market	        str	    Y	    市场类型（主板/创业板/科创板/CDR）
+                exchange	    str	    N	    交易所代码
+                curr_type	    str	    N	    交易货币
+                list_status	    str	    N	    上市状态 L上市 D退市 P暂停上市
+                list_date	    str	    Y	    上市日期
+                delist_date	    str	    N	    退市日期
+                is_hs	        str	    N	    是否沪深港通标的，N否 H沪股通 S深股通
+                act_name	    str	    Y	    实控人名称
+                act_ent_type	str	    Y	    实控人企业性质
         """
         pass
