e12d8dadcad6a790841eb6c9d92f5866db5a7f0d
Jack <jack4shihong@gmail.com>
2025-04-25 15:12:15 +0800

    add continue up times

--- src/analysis/up_limit_times.py
+++ src/analysis/up_limit_times.py
@@ -1,5 +1,5 @@
 """
-up_limit_times.py 计算全市场股票在过去一年的涨停次数
+up_limit_times.py 计算全市场股票在过去一年的涨停和连板次数
 
     @Time    : 2025/04/23
     @Author  : JackWang
@@ -33,7 +33,7 @@ proxy = TuShareProxy(config)
 
 
 def up_limit_times(ts_code: str, start_time: datetime, end_time: datetime) -> int:
-    """up_limit_times 计算指定股票在指定时间范围内的涨停次数"""
+    """up_limit_times 计算指定股票在指定时间范围内的涨停和连板次数"""
 
     daily_data = proxy.daily(
         ts_code=ts_code,
@@ -45,9 +45,15 @@ def up_limit_times(ts_code: str, start_time: datetime, end_time: datetime) -> in
         ts_code=ts_code, start_date=start_time.strftime("%Y%m%d")
     ).set_index("trade_date")
 
-    data = concat_df(daily_data, limit_data, remove_duplicates=True)
+    data = concat_df(
+        daily_data, limit_data.reindex(daily_data.index), remove_duplicates=True
+    )
 
-    return (data["up_limit"] == data["close"]).sum()
+    up_limit_mask = data["up_limit"] == data["close"]
+    groups = (~up_limit_mask).cumsum()
+    true_blocks = up_limit_mask.groupby(groups).sum()
+
+    return up_limit_mask.sum(), true_blocks.max()
 
 
 def main():
@@ -82,7 +88,7 @@ def main():
                 advance=1,
                 description=f"计算 [cyan]{ts_code}: {name}[/cyan] 的涨停次数",
             )
-            up_times = up_limit_times(
+            up_times, max_continue_up_times = up_limit_times(
                 ts_code=ts_code,
                 start_time=get_relative_trade_day(
                     end_date=datetime.now(),
@@ -97,11 +103,12 @@ def main():
                     "ts_code": ts_code,
                     "name": name,
                     "up_limit_times": up_times,
+                    "max_continue_up_times": max_continue_up_times,
                 }
             )
 
             print(
-                f"{ts_code}         [bold]{name}[/bold], 涨停次数: [green]{up_times}[/green]"
+                f"{ts_code}         [bold]{name}[/bold], 涨停次数: [green]{up_times}[/green], 最大连板次数: [green]{max_continue_up_times}[/green]"
             )
 
     result = pd.DataFrame(result).sort_values(by="up_limit_times", ascending=False)
